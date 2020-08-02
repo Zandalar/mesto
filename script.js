b31a47a88105ff98c60a9191b23490f586214946
Fernandes <vs.masich@gmail.com>
2020-08-03 02:10:17 +0300

    JS форма.

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-let editProfile = document.querySelector('.profile__info_button-edit');
 let popup = document.querySelector('.popup');
-let closeProfile = document.querySelector('.popup__button-close');
+let editProfile = document.querySelector('.profile__info_button-edit');
+let closeProfile = popup.querySelector('.popup__button-close');
+let saveProfile = popup.querySelector('.popup__button-save');
+let profileName = document.querySelector('.profile__info_name');
+let profileAbout = document.querySelector('.profile__info_about');
+let newProfileName = document.querySelector('#popup__field_name');
+let newProfileAbout = document.querySelector('#popup__field_about');
 
 function popupOpened() {
-  popup.classList.add('popup_opened');
+  popup.classList.toggle('popup_opened');
+  newProfileName.value = profileName.textContent;
+  newProfileAbout.value = profileAbout.textContent;
 }
 
-function popupClosed() {
-  popup.classList.remove('popup_opened');
+function popupSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = newProfileName.value;
+  profileAbout.textContent = newPofileAbout.value;
 }
 
 editProfile.addEventListener('click', popupOpened);
-closeProfile.addEventListener('click', popupClosed);
+closeProfile.addEventListener('click', popupOpened);
+saveProfile.addEventListener('submit', popupSubmitHandler);
+
+
 
 
