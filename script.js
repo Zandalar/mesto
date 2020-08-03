18c55a19e6422c3466e8452cc05ad78ee4d5de10
Fernandes <vs.masich@gmail.com>
2020-08-03 20:40:16 +0300

    hotfixes: мелкие правки по всему проекту

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ let closeProfile = popup.querySelector('.popup__button-close');
 let saveProfile = popup.querySelector('.popup__button-save');
 let profileName = document.querySelector('.profile__info_name');
 let profileAbout = document.querySelector('.profile__info_about');
-let newProfileName = document.querySelector('#popup__field_name');
-let newProfileAbout = document.querySelector('#popup__field_about');
+let newProfileName = popup.querySelector('#popup__field_name');
+let newProfileAbout = popup.querySelector('#popup__field_about');
+let popup__form = popup.querySelector('.popup__form');
 
 function popupOpened() {
   popup.classList.toggle('popup_opened');
@@ -13,15 +14,16 @@ function popupOpened() {
   newProfileAbout.value = profileAbout.textContent;
 }
 
-function popupSubmitHandler(evt) {
-  evt.preventDefault();
+function popupSubmitHandler(event) {
   profileName.textContent = newProfileName.value;
-  profileAbout.textContent = newPofileAbout.value;
+  profileAbout.textContent = newProfileAbout.value;
+  popupOpened();
+  event.preventDefault();
 }
 
 editProfile.addEventListener('click', popupOpened);
 closeProfile.addEventListener('click', popupOpened);
-saveProfile.addEventListener('submit', popupSubmitHandler);
+popup__form.addEventListener('submit', popupSubmitHandler);
 
 
 
