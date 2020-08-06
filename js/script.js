540dde663df8c8896e59b99ba58477999e82e183
Fernandes <vs.masich@gmail.com>
2020-08-06 23:41:38 +0300

    feature: новые попапы. Рефакторинг JS

--- js/script.js
+++ js/script.js
@@ -1,30 +1,51 @@
 let popup = document.querySelector('.popup');
-let editProfile = document.querySelector('.profile__info_button-edit');
-let closeProfile = popup.querySelector('.popup__button-close');
-let saveProfile = popup.querySelector('.popup__button-save');
+let profileForm = popup.querySelector('#profile__form');
+let placeForm = popup.querySelector('#place__form');
+let profileContainer = document.querySelector('#popup__profile');
+let placeContainer = document.querySelector('#popup__place');
+
+let editButton = document.querySelector('.profile__info_button-edit');
+let addCardButton = document.querySelector('.profile__button-add');
+let closeButton = document.querySelector('.popup__button-close');
+let saveProfileButton = popup.querySelector('.popup__button-save');
+
 let profileName = document.querySelector('.profile__info_name');
 let profileAbout = document.querySelector('.profile__info_about');
-let newProfileName = popup.querySelector('#popup__field_name');
-let newProfileAbout = popup.querySelector('#popup__field_about');
-let popup__form = popup.querySelector('.popup__form');
+let newProfileName = popup.querySelector('#profile__name');
+let newProfileDescription = popup.querySelector('#profile__description');
 
-function popupOpened() {
-  popup.classList.toggle('popup_opened');
+function togglePopup(container) {
+  container.classList.toggle('popup_opened');
+}
+
+function fillProfileForm() {
   newProfileName.value = profileName.textContent;
-  newProfileAbout.value = profileAbout.textContent;
+  newProfileDescription.value = profileAbout.textContent;
 }
 
-function popupSubmitHandler(event) {
-  profileName.textContent = newProfileName.value;
-  profileAbout.textContent = newProfileAbout.value;
-  popupOpened();
+function saveProfileForm(event) {
   event.preventDefault();
+  profileName.textContent = newProfileName.value;
+  profileAbout.textContent = newProfileDescription.value;
+  togglePopup(profileContainer);
 }
 
-editProfile.addEventListener('click', popupOpened);
-closeProfile.addEventListener('click', popupOpened);
-popup__form.addEventListener('submit', popupSubmitHandler);
+function closePopup() {
+  let closeButtonSelector = document.querySelector('.');
+  if (closeButtonSelector.classList.contains('popup_opened')) {
+    closeButtonSelector.classList.remove('popup_opened');
+  }
+}
 
+editButton.addEventListener('click', () => {
+  togglePopup(profileContainer);
+  fillProfileForm();
+})
 
+closeButton.addEventListener('click', closePopup);    //хочу повесить на один обработчик события все кнопки закрытия(нужно прописать логику поиска открытого попапа)
 
+addCardButton.addEventListener('click', () => {
+  togglePopup(placeContainer);
+})
 
+profileForm.addEventListener('submit', saveProfileForm);
