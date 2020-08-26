55db40a31cce6b7c54ddf0ab9ef4dc7fca7219b3
Fernandes <vs.masich@gmail.com>
2020-08-26 21:52:08 +0300

    feature: complete validation JS

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,80 +1,3 @@
-/* const showInputError = (formElement, inputElement, errorMessage) => {
-  // Находим элемент ошибки внутри самой функции
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  // Остальной код такой же
-  inputElement.classList.add('popup__field_type_error');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__field-error_active');
-};
-
-const hideInputError = (formElement, inputElement) => {
-  // Находим элемент ошибки
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  // Остальной код такой же
-  inputElement.classList.remove('form__field_type_error');
-  errorElement.classList.remove('form__field-error_active');
-  errorElement.textContent = '';
-};
-
-// Функция isValid теперь принимает formElement и inputElement,
-// а не берёт их из внешней области видимости
-
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    // showInputError теперь получает параметром форму, в которой
-    // находится проверяемое поле, и само это поле
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    // hideInputError теперь получает параметром форму, в которой
-    // находится проверяемое поле, и само это поле
-    hideInputError(formElement, inputElement);
-  }
-};
-
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.form__field'));
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-    });
-  });
-};
-
-const enableValidation = () => {
-  // Найдём все формы с указанным классом в DOM,
-  // сделаем из них массив методом Array.from
-  const formList = Array.from(document.querySelectorAll('.popup__form'));
-
-  // Переберём полученную коллекцию
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', (evt) => {
-      // У каждой формы отменим стандартное поведение
-      evt.preventDefault();
-    });
-
-    // Для каждой формы вызовем функцию setEventListeners,
-    // передав ей элемент формы
-    setEventListeners(formElement);
-  });
-};
-
-// Вызовем функцию
-enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__field',
-  submitButtonSelector: '.popup__button-save',
-  inactiveButtonClass: 'popup__button-save_disabled',
-  inputErrorClass: 'popup__field_type_error',
-  errorClass: 'popup__field-error_active'
-});
-*/
-/*
-const formElement = document.querySelector('.popup__form');
-const formInput = formElement.querySelector('.popup__field');
-const formError = formElement.querySelector(`#${formInput.id}-error`);
-*/
-
-
 const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
 
@@ -101,9 +24,12 @@ const isValid = (formElement, inputElement) => {
 
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.popup__field'));
+  const buttonElement = formElement.querySelector('.popup__button-save');
+  toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
       isValid(formElement, inputElement);
+        toggleButtonState(inputList, buttonElement);
     })
   })
 }
@@ -118,17 +44,20 @@ const enableValidation = () => {
     })
 }
 
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__button-save_disabled');
+    buttonElement.setAttribute('disabled', 'disabled');
+  } else {
+    buttonElement.classList.remove('popup__button-save_disabled');
+    buttonElement.removeAttribute('disabled', 'disabled');
+  }
+};
+
 enableValidation();
-
-
-
-
-/*
-formElement.addEventListener('input', (evt) => {
-  evt.preventDefault();
-})
-*/
-
-/*
-formInput.addEventListener('input', isValid);
-*/
